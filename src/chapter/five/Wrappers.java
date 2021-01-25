24033747744aadf5060151b02f2b48830ede656a
Adam Balazs <balazsa@starschema.net>
2021-01-25 23:37:56 +0100

    5 ...

---
+++ src/chapter/five/Wrappers.java
@@ -0,0 +1,60 @@
+package chapter.five;
+
+public class Wrappers {
+	public static void main(String[] args) {
+		{
+			Boolean aBoolean = Boolean.valueOf(true);
+			Byte aByte = Byte.valueOf((byte) 1);
+			Short aShort = Short.valueOf((short) 1);
+			Integer integer = Integer.valueOf(1);
+			Long aLong = Long.valueOf(1);
+			Long aLong1 = Long.valueOf(1L);
+			Float aFloat = Float.valueOf(1);
+			Float aFloat1 = Float.valueOf(1f);
+			Double aDouble = Double.valueOf(1);
+			Double aDouble1 = Double.valueOf(1d);
+			Character character = Character.valueOf('1');
+			System.out.println();
+		}
+		{
+			Boolean aTrue = Boolean.valueOf("true");
+			Byte aByte = Byte.valueOf("1");
+			Short aShort = Short.valueOf("1");
+			Integer integer = Integer.valueOf("1");
+			Long aLong = Long.valueOf("1");
+			//Long aLong1 = Long.valueOf("1L"); //throws NumberFormatException
+			Float aFloat = Float.valueOf("1"); // 1.0
+			Float aFloat1 = Float.valueOf("1f"); // 1.0
+			Float aFloat2 = Float.valueOf("1d"); // 1.0
+			Float aFloat3 = Float.valueOf("1."); // 1.0
+			Float aFloat4 = Float.valueOf("1.0"); // 1.0
+			Double aDouble = Double.valueOf("1"); // 1.0
+			Double aDouble1 = Double.valueOf("1f"); // 1.0
+			Double aDouble2 = Double.valueOf("1d"); // 1.0
+			Double aDouble3 = Double.valueOf("1."); // 1.0
+			Double aDouble4 = Double.valueOf("1.0"); // 1.0
+			Character character = "1".charAt(0);
+			System.out.println();
+		}
+		{
+			boolean aTrue = Boolean.parseBoolean("true");
+			byte b = Byte.parseByte("1");
+			short i = Short.parseShort("1");
+			int i1 = Integer.parseInt("1");
+			long l = Long.parseLong("1");
+			//long l1 = Long.parseLong("1L"); //throws NumberFormatException
+			float v = Float.parseFloat("1"); // 1.0
+			float v1 = Float.parseFloat("1f"); // 1.0
+			float v2 = Float.parseFloat("1d"); // 1.0
+			float v3 = Float.parseFloat("1."); // 1.0
+			float v4 = Float.parseFloat("1.0"); // 1.0
+			double v5 = Double.parseDouble("1"); // 1.0
+			double v6 = Double.parseDouble("1f"); // 1.0
+			double v7 = Double.parseDouble("1d"); // 1.0
+			double v8 = Double.parseDouble("1."); // 1.0
+			double v9 = Double.parseDouble("1.0"); // 1.0
+			char c = "1".charAt(0);
+			System.out.println("");
+		}
+	}
+}
